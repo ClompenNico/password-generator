f6a5bd53cbbd4b95871ba0d436b6b1838238579e
ClompenNico <32330319+ClompenNico@users.noreply.github.com>
2018-04-27 10:18:20 +0200

    mappen structuur opgezet

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+// referenties leggen naar de tools (die geïnstalleerd zijn)
+let gulp = require("gulp"),
+    sourcemaps = require("gulp-sourcemaps"),
+    authoprefixer = require("gulp-autoprefixer"),
+    cleanCSS = require("gulp-clean-css"),
+    ccslint = require("gulp-csslint"),
+    jshint = require("gulp-jshint"),
+    jsStylish = require("jshint-stylish"),
+    uglify = require("gulp-uglify"),
+    notify = require("gulp-notify"),
+    concat = require("gulp-concat"),
+    sass = require("gulp-sass");
+
+const PATHS = {
+    HTML : {
+        SRC : './src/*.html',
+        DIST: './dist/',
+    },
+    JS : {
+        SRC : './src/js/**/*.js',    //Die twee sterren is voor submappen ook mee te nemen!
+        DIST: './dist/js/'
+    },
+    CSS: {
+        SRC : './src/css/**/screen.scss',
+        DIST: './dist/css/'
+    }
+};
+
+gulp.task("default", function(){
+    //watch
+    let htmlWatcher = gulp.watch(PATHS.HTML.SRC, ['copy-html']);
+    htmlWatcher.on('change', function(event){
+        console.log("File " + event.path + " was " + event.type);
+    })
+
+    gulp.watch(PATHS.JS.SRC, ['js']);
+    gulp.watch(PATHS.CSS.SRC, ['css']);
+});
+
+gulp.task("js", function(){
+    gulp.src(PATHS.JS.SRC)
+        .pipe(concat("app.js"))
+        .pipe(gulp.dest(PATHS.JS.DIST));
+});
+
+gulp.task("css", function(){
+    gulp.src(PATHS.CSS.SRC)
+    .pipe(concat("app.css"))
+    .pipe(gulp.dest(PATHS.CSS.DIST));
+
+});
+
+gulp.task("copy-html", function(){
+    gulp.src(PATHS.HTML.SRC)
+        .pipe(gulp.dest(PATHS.HTML.DIST));
+});
